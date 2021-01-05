410167e3f238ee551f577a328fd3f76b706f7803
Jorge Montes <jorgemontes771@gmail.com>
2021-01-05 14:52:26 -0500

    toggle with components working

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,4 +1,40 @@
-const appRoot = document.getElementById('app');
+class VisibilityToggle extends React.Component{
+    constructor(props){
+        super(props);
+        this.handleTogleVisibility = this.handleTogleVisibility.bind(this);
+        this.state ={
+            visibility : false
+        };
+    }
+
+    handleTogleVisibility(){
+        this.setState((prevState) =>{
+            return{
+                visibility : !prevState.visibility
+            };
+        });
+    }
+   
+    render(){
+        return(
+            <div>
+            <h1>Visibility Toggle</h1>
+            <button onClick={this.handleTogleVisibility}>
+            {this.state.visibility ? 'Hide details' : 'Show details'}
+            </button>
+            {this.state.visibility && (
+                <div>
+                <p>If you are seeing this, you are on the right track!!!</p>
+                </div>
+            )}
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<VisibilityToggle/>, document.getElementById('app'));
+
+/*const appRoot = document.getElementById('app');
 
 let visibility = false;
 
@@ -24,4 +60,4 @@ let visible = (
     ReactDOM.render(visible, appRoot);
 };
 
-render();
+render();*/
